03913be7b73993172c99bc29f03fcb990417df6c
vasivik <vasivik@gmail.com>
2019-09-13 17:41:50 +0700

    add lib folder for exted classes, add autofall bricks

---
+++ lib/brick.rb
@@ -0,0 +1,82 @@
+class Brick
+
+  BOTTOM_LINE = 400
+  SPAWN_LINE = 70
+  BRICK_SIZE = [60, 60].freeze
+  FALL_STEP = 60
+  BUTTON_SLEEP_TIME = 0.2
+
+  attr_accessor :id, :last_turn_left, :last_turn_right, :last_turn_down
+
+  def initialize
+    @id = "#{Time.now.to_i}_#{rand(5_000)}"
+    @brick = Gosu::Image.new("media/brick.png")
+    @x = @y = 0.0
+    @last_turn_left = @last_turn_right = @last_turn_down = Time.now.to_f
+  end
+
+  def turn_left
+    return if cannot_use_button?(@last_turn_left)
+    @last_turn_left = Time.now.to_f
+    @x -= FALL_STEP
+    @x = 0 if @x <= 0
+  end
+
+  def turn_right
+    return if cannot_use_button?(@last_turn_right)
+    @last_turn_right = Time.now.to_f
+    @x += FALL_STEP
+    @x = WIDTH if @x >= WIDTH
+  end
+
+  def turn_down
+    return if cannot_use_button?(@last_turn_down)
+    @last_turn_down = Time.now.to_f
+    @y += FALL_STEP
+  end
+
+  def x_range
+    [@x - BRICK_SIZE[0] / 2, @x + BRICK_SIZE[0] / 2]
+  end
+
+  def y_range
+    [@y - BRICK_SIZE[1] / 2, @y + BRICK_SIZE[1] / 2]
+  end
+
+  def current_coordinate
+    "#{@x} #{@y}"
+  end
+
+  def drop_down?
+    @y > BOTTOM_LINE
+  end
+
+  def in_spawn_zone?
+    @y < SPAWN_LINE
+  end
+
+  def go_home
+    @x = @y = 0
+  end
+
+  def touched_with(current_brick)
+    ((current_brick.x_range[0] + 1).between?(x_range[0], x_range[1]) ||
+      (current_brick.x_range[1] - 1).between?(x_range[0], x_range[1])) &&
+      (current_brick.y_range[0].between?(y_range[0], y_range[1]) ||
+        current_brick.y_range[1].between?(y_range[0], y_range[1]))
+  end
+
+  def fall_on_step
+    @y += FALL_STEP
+  end
+
+  def draw
+    @brick.draw_rot(@x, @y, 0, 0)
+  end
+
+  private
+
+  def cannot_use_button?(last_time)
+    (last_time + BUTTON_SLEEP_TIME) >= Time.now.to_f
+  end
+end
--- start_game.rb
+++ start_game.rb
@@ -1,88 +1,27 @@
 require 'gosu'
-
-class Brick
-
-  BOTTOM_LINE = 400
-  SPAWN_LINE = 70
-  BRICK_SIZE = [60, 60].freeze
-
-  attr_accessor :id
-
-  def initialize
-    @id = "#{Time.now.to_i}_#{rand(5_000)}"
-    @brick = Gosu::Image.new("media/brick.png")
-    @x = @y = 0.0
-  end
-
-  def turn_left
-    @x -= 1
-  end
-
-  def turn_right
-    @x += 1
-  end
-
-  def turn_up
-    @y -= 1
-  end
-
-  def turn_down
-    @y += 1
-  end
-
-  def x_range
-    [@x - BRICK_SIZE[0] / 2, @x + BRICK_SIZE[0] / 2]
-  end
-
-  def y_range
-    [@y - BRICK_SIZE[1] / 2, @y + BRICK_SIZE[1] / 2]
-  end
-
-  def current_coordinate
-    "#{@x} #{@y}"
-  end
-
-  def drop_down?
-    @y > BOTTOM_LINE
-  end
-
-  def in_spawn_zone?
-    @y < SPAWN_LINE
-  end
-
-  def go_home
-    @x = @y = 0
-  end
-
-  def touched_with(current_brick)
-    (current_brick.x_range[0].between?(x_range[0], x_range[1]) || current_brick.x_range[1].between?(x_range[0], x_range[1])) &&
-      (current_brick.y_range[0].between?(y_range[0], y_range[1]) || current_brick.y_range[1].between?(y_range[0], y_range[1]))
-  end
-
-  def draw
-    @brick.draw_rot(@x, @y, 0, 0)
-  end
-end
+require_relative 'lib/brick'
+WIDTH = 660
 
 class RubyTetris < Gosu::Window
+  FALL_TIME = 0.5
+  attr_accessor :fall_time
+
   def initialize
-    super 640, 480
+    super WIDTH, 480
     self.caption = "RubyTetris Game"
     @bricks = [Brick.new] #
     @coordinate_text = Gosu::Font.new(20)
     set_current_brick
+    @fall_time = Time.now.to_f
   end
 
   def update
-    @current_brick.turn_left if Gosu.button_down? Gosu::KB_LEFT or Gosu.button_down? Gosu::GP_LEFT
-    @current_brick.turn_right if Gosu.button_down? Gosu::KB_RIGHT or Gosu.button_down? Gosu::GP_RIGHT
-    @current_brick.turn_up if Gosu.button_down? Gosu::KB_UP or Gosu.button_down? Gosu::GP_UP
+    @current_brick.turn_left if Gosu.button_down? Gosu::KB_LEFT # or Gosu.button_down? Gosu::GP_LEFT
+    @current_brick.turn_right if Gosu.button_down? Gosu::KB_RIGHT #  or Gosu.button_down? Gosu::GP_RIGHT
     @current_brick.turn_down if Gosu.button_down? Gosu::KB_DOWN or Gosu.button_down? Gosu::GP_DOWN
     check_bricks
     set_current_brick
-    p ' = ' * 20
-    p Time.now.seconds
-    p ' = ' * 20
+    fall_every_second
   end
 
   def check_bricks
@@ -104,6 +43,12 @@ class RubyTetris < Gosu::Window
       return true if brick.touched_with(@current_brick)
     end
     false
+  end
+
+  def fall_every_second
+    return if (@fall_time + FALL_TIME) >= Time.now.to_i
+    @fall_time = Time.now.to_f
+    @current_brick.fall_on_step
   end
 
   def draw
